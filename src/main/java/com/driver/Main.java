ca38292b288985c038bca3a3fb2d0c0488fe472f
Ayush Singh <protonm011@pm.me>
2022-12-13 15:52:31 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj= new RWOnly();
+
+//        'name' has private access in 'com.driver.RWOnly'
+//        obj.name="Ayush";
+
+        obj.setName("proton");
+        System.out.println(obj.getName());
+    }
   
 }
